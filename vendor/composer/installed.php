a63f5ae457df4965098c214be03f4cd41c83fa17
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-09-02 12:04:47 +0100

    wip

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'abdiel/guesser',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a97053d135b19725d046d284419d72c23111cef4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'abdiel/guesser' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a97053d135b19725d046d284419d72c23111cef4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
